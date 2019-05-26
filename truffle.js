d14849d285c2d7a113c614f4114df7004f4d7d2e
sic616 <sic61695@gmail.com>
2019-05-26 20:36:48 +0900

    Klaytn Inflearn

--- truffle.js
+++ truffle.js
@@ -1,1 +1,17 @@
 // truffle.js config for klaytn.
+const PrivateKeyConnector = require('connect-privkey-to-provider')
+const NETWORK_ID = '1001'
+const GASLIMIT = '20000000'
+const URL = 'https://api.baobab.klaytn.net:8651'
+const PRIVATE_KEY = '0x24d2a66d486d29294a70ba2f762623ba8f6d71be806178567069006882af825b'
+
+module.exports = {
+    networks: {
+        klaytn: {
+            provider: new PrivateKeyConnector(PRIVATE_KEY, URL),
+            network_id: NETWORK_ID,
+            gas: GASLIMIT,
+            gasPrice: null,
+        }
+    },
+}
